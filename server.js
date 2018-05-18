2d057b2ea1a4d21de63515e7bde7428e52265fdc
Mohammad <mohammad8758@hotmail.com>
2018-05-19 01:02:01 +0530

    som'n

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 
 var app = express();
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/login-portal')
+    .then(() => console.log('Mongoose Connected'))
+    .catch(e => console.log(e));
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');

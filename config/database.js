8ad19ad83478a035560141d42e7011527dbddb42
Mohammad <mohammad8758@hotmail.com>
2018-05-25 17:55:35 +0530

    fixed logout

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: ''}
+    module.exports = {mongoURI: 'mongodb://mohammad:qazwsx@ds227740.mlab.com:27740/login-portal'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/login-portal'}
 }

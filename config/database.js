f66edd6c7e8098f31bc2735c99e320ea5776854a
mohammad-23 <38518492+mohammad-23@users.noreply.github.com>
2018-09-03 05:13:16 +0530

    Delete database.js

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb://mohammad:qazwsx@ds227740.mlab.com:27740/login-portal'}
-} else {
-    module.exports = {mongoURI: 'mongodb://localhost/login-portal'}
-}
